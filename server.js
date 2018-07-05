e1ac86497fc00fe727143622459d9cfd048b55b9
Xavi <murciaxavi@gmail.com>
2018-07-06 00:00:49 +0200

    Node server config updated

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/wefox-project'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/wefox-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
